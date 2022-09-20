75fcc4a3206d94fba59cb403b4015a9187f73919
pablodamascenoo <pablofacex@gmail.com>
2022-09-20 17:13:18 -0300

    feat: added favicon and font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}", "index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Lato", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
