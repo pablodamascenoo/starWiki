85e69713af91f951ab81ada6cac0ca20f3fa02fe
pablodamascenoo <pablofacex@gmail.com>
2022-09-22 14:14:40 -0300

    fix: changed Card component

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,11 +1,7 @@
-import Tilt from "react-vanilla-tilt";
-
 export default function Card(props) {
   return (
-    <Tilt>
-      <div className="border-[#ffffff] border-2 w-60 h-64 rounded-lg p-4">
-        {props.children}
-      </div>
-    </Tilt>
+    <div className="border-[#ffffff] border-2 w-60 h-64 rounded-lg p-4">
+      {props.children}
+    </div>
   );
 }
